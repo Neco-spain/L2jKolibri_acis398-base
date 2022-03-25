d120d38e2f56dc39271611402815c50bd2da5c38
Administrator <klevis@resulaj.eu>
2022-03-25 23:45:57 +0100

    repaired voiced commands due to some mistakes for overwriting
    Signed-off-by: Administrator <klevis@resulaj.eu>

--- aCis/aCis_gameserver/java/net/sf/l2j/gameserver/handler/VoicedCommandHandler.java
+++ aCis/aCis_gameserver/java/net/sf/l2j/gameserver/handler/VoicedCommandHandler.java
@@ -5,61 +5,61 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 import net.sf.l2j.gameserver.GameServer;
+import net.sf.l2j.gameserver.handler.voicedcommandhandlers.Menu;
+import net.sf.l2j.gameserver.handler.voicedcommandhandlers.Ranking;
+import net.sf.l2j.gameserver.handler.voicedcommandhandlers.Shiff_Mod;
+import net.sf.l2j.gameserver.handler.voicedcommandhandlers.TvTEventCommand;
 
-public class VoicedCommandHandler
-{
+public class VoicedCommandHandler {
 	private static Logger LOGGER = Logger.getLogger(GameServer.class.getName());
-	
+
 	private static VoicedCommandHandler _instance;
-	
+
 	private final Map<String, IVoicedCommandHandler> _datatable;
-	
-	public static VoicedCommandHandler getInstance()
-	{
-		if (_instance == null)
-		{
+
+	public static VoicedCommandHandler getInstance() {
+		if (_instance == null) {
 			_instance = new VoicedCommandHandler();
 		}
-		
+
 		return _instance;
 	}
-	
-	private VoicedCommandHandler()
-	{
-		_datatable = new HashMap<>();
 
-		//registerVoicedCommandHandler(new Shiff_Mod());
-		
+	private VoicedCommandHandler() {
+		_datatable = new HashMap<>();
+		// registrar comando aqui exemplo abaixo
+		// registerVoicedCommandHandler(new Shiff_Mod());
+		registerVoicedCommandHandler(new Shiff_Mod());
+		registerVoicedCommandHandler(new TvTEventCommand());
+		registerVoicedCommandHandler(new Ranking());
+		registerVoicedCommandHandler(new Menu());
+
 		LOGGER.info("VoicedCommandHandler: Loaded " + _datatable.size() + " handlers.");
 	}
-	
-	public void registerHandler(final IVoicedCommandHandler handler)
-	{
+
+	public void registerVoicedCommandHandler(final IVoicedCommandHandler handler) {
 		String[] ids = handler.getVoicedCommandList();
-		
-		for (final String id : ids)
-		{
+
+		for (final String id : ids) {
 			_datatable.put(id, handler);
 		}
-		
+
 		ids = null;
 	}
-	
-	public IVoicedCommandHandler getVoicedCommandHandler(final String voicedCommand)
-	{
+
+	public IVoicedCommandHandler getVoicedCommandHandler(final String voicedCommand) {
 		String command = voicedCommand;
-		
-		if (voicedCommand.indexOf(" ") != -1)
-		{
+
+		if (voicedCommand.indexOf(" ") != -1) {
 			command = voicedCommand.substring(0, voicedCommand.indexOf(" "));
 		}
 		return _datatable.get(command);
 	}
+
 	/**
 	 * @return
 	 */
-	public int size()
-	{
+	public int size() {
 		return _datatable.size();
 	}
 }
