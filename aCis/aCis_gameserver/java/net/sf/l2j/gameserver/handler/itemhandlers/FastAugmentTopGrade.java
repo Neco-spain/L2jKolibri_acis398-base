cb5952a2fe5a8ed2d22cd675dbe5a40c0f2cb8d0
Kolibri <klevis@resulaj.eu>
2022-04-14 23:59:15 +0200

    REMOVED sysdlg again
    Signed-off-by: Kolibri <klevis@resulaj.eu>

--- aCis/aCis_gameserver/java/net/sf/l2j/gameserver/handler/itemhandlers/FastAugmentTopGrade.java
+++ aCis/aCis_gameserver/java/net/sf/l2j/gameserver/handler/itemhandlers/FastAugmentTopGrade.java
@@ -1,7 +1,5 @@
 package net.sf.l2j.gameserver.handler.itemhandlers;
 
-
-import net.sf.l2j.commons.pool.ThreadPool;
 import net.sf.l2j.gameserver.data.xml.AugmentationData;
 import net.sf.l2j.gameserver.enums.Paperdoll;
 import net.sf.l2j.gameserver.enums.SayType;
@@ -10,8 +8,6 @@ import net.sf.l2j.gameserver.model.Augmentation;
 import net.sf.l2j.gameserver.model.actor.Playable;
 import net.sf.l2j.gameserver.model.actor.Player;
 import net.sf.l2j.gameserver.model.item.instance.ItemInstance;
-import net.sf.l2j.gameserver.network.SystemMessageId;
-import net.sf.l2j.gameserver.network.serverpackets.ConfirmDlg;
 import net.sf.l2j.gameserver.network.serverpackets.CreatureSay;
 import net.sf.l2j.gameserver.network.serverpackets.ExShowScreenMessage;
 import net.sf.l2j.gameserver.network.serverpackets.InventoryUpdate;
@@ -29,67 +25,41 @@ public class FastAugmentTopGrade implements IItemHandler {
 
 		} else if (weap.getItem().getCrystalType().getId() <= 3) {
 			player.sendMessage("You can only fast augment on A and S grade  Weapons!");
-//
-
+			return;
 		} else if (weap.isHeroItem()) {
 			player.sendMessage("You can't add Augment on " + weap.getItemName() + " !");
 
 		} else if (weap.isAugmented()) {
-
-			boolean isPassive = weap.getAugmentation().getSkill().isPassive()&& !weap.getAugmentation().getSkill().isChance();
-			boolean isActive = weap.getAugmentation().getSkill().isActive();
-			boolean isChance = weap.getAugmentation().getSkill().isChance();
-			if (weap.getAugmentation().getSkill() == null) {
-				weap.getAugmentation().removeBonus(player);
-				weap.removeAugmentation(true);
-				InventoryUpdate iu = new InventoryUpdate();
-				iu.addModifiedItem(weap);
-				player.sendPacket(iu);
-				player.broadcastUserInfo();
-			}
-			else if (isActive||isPassive ||isChance){
-				sendDlg(player,playable);
-			}
-
-		} else {
-			player.destroyItem("Consume", item.getObjectId(), 1, null, false);
-			weap.setAugmentation(aug);
-
+			weap.getAugmentation().removeBonus(player);
+			weap.removeAugmentation(true);
 			InventoryUpdate iu = new InventoryUpdate();
 			iu.addModifiedItem(weap);
 			player.sendPacket(iu);
 			player.broadcastUserInfo();
+		} else {
+			player.destroyItem("Consume", item.getObjectId(), 1, null, false);
+			weap.setAugmentation(aug);
+			InventoryUpdate iu = new InventoryUpdate();
+			iu.addModifiedItem(weap);
+			player.sendPacket(iu);
+			player.broadcastUserInfo();
+
 			if (weap.getAugmentation().getSkill() == null) {
 				player.sendMessage("No luck try again!");
 			} else
-				checkAugment(playable);
+				checkaugment(playable, weap);
 		}
 	}
 
-		private void sendDlg(Player player , Playable playable) {
-			ItemInstance weap = playable.getInventory().getItemFrom(Paperdoll.RHAND);
-			String name = weap.getAugmentation().getSkill().getName();
-
-
-
-
-			ConfirmDlg confirm = new ConfirmDlg(SystemMessageId.S1.getId());
-			confirm.addString("Do you wish to remove " + name + "augment ?");
-			confirm.addTime(10000);
-			weap.removeAugmentation(true);
-			ThreadPool.schedule(() -> weap.removeAugmentation(false), 10000);
-			player.sendPacket(confirm);
-		}
-
-
-	private static void checkAugment(Playable playable) {
+	private static void checkaugment(Playable playable, ItemInstance item) {
 		Player player = (Player) playable;
 		ItemInstance weap = playable.getInventory().getItemFrom(Paperdoll.RHAND);
 		String name = weap.getAugmentation().getSkill().getName();
-		boolean isPassive = weap.getAugmentation().getSkill().isPassive()&& !weap.getAugmentation().getSkill().isChance();
-		boolean isActive = weap.getAugmentation().getSkill().isActive();
+		boolean isPassive = weap.getAugmentation().getSkill().isPassive()
+				&& !weap.getAugmentation().getSkill().isChance();
+		boolean isactive = weap.getAugmentation().getSkill().isActive();
 		boolean isChance = weap.getAugmentation().getSkill().isChance();
-		if (isChance) {
+		if (isChance == true) {
 			player.sendPacket(new CreatureSay(0, SayType.HERO_VOICE, "[CHANCE]", "You got " + name));
 			sendMsg(player, "CHANCE : " + "You got " + name);
 			player.disarmWeapon(true);
@@ -98,7 +68,7 @@ public class FastAugmentTopGrade implements IItemHandler {
 			player.sendPacket(iu);
 			player.broadcastUserInfo();
 		}
-		if (isActive) {
+		if (isactive == true) {
 			player.sendPacket(new CreatureSay(0, SayType.HERO_VOICE, "[ACTIVE]", "You got " + name));
 			sendMsg(player, "ACTIVE : " + "You got " + name);
 			player.disarmWeapon(true);
@@ -107,7 +77,7 @@ public class FastAugmentTopGrade implements IItemHandler {
 			player.sendPacket(iu);
 			player.broadcastUserInfo();
 		}
-		if (isPassive) {
+		if (isPassive == true) {
 			player.sendPacket(new CreatureSay(0, SayType.HERO_VOICE, "[PASSIVE]", "You got " + name));
 			sendMsg(player, "PASSIVE : " + "You got " + name);
 			player.disarmWeapon(true);
@@ -116,7 +86,9 @@ public class FastAugmentTopGrade implements IItemHandler {
 			player.sendPacket(iu);
 			player.broadcastUserInfo();
 		}
+		return;
 	}
+
 	private static void sendMsg(final Player player, final String s) {
 		player.sendPacket(new ExShowScreenMessage(s, 3000, ExShowScreenMessage.SMPOS.TOP_CENTER, true));
 		player.sendMessage(s);
